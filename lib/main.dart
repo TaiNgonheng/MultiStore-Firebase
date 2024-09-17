8431739021c2a942526c15d39ee9651671e8b4b5
TaiNgonheng <heng68807@gmail.com>
2024-09-17 17:30:06 +0700

    add debugshowbanner under to false underthe material app to take away debug red screen on emulator

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(statusBarColor: Colors.transparent));
     return MaterialApp(
+
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
