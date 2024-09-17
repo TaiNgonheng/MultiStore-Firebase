a7a1e396d003da28fadaf022e1f8ce5f1f42b6b9
TaiNgonheng <heng68807@gmail.com>
2024-09-17 17:28:41 +0700

    delete const before scaffold to use bottomNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,6 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
- // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
