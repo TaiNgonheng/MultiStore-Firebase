da43c89324120de0d97b173d82fa86a17ccfd46c
TaiNgonheng <heng68807@gmail.com>
2024-09-17 17:58:11 +0700

    bottomnavigationBar: BottomNavigationBar( unselecteditemcolor: Colors.black, selecteditemcolor: Colors.blue, item=[  BottomNavigationBarItem(icon: Icon(CupertinoIcons.home), label: 'HOME', ] )

--- lib/views/buyers/main_screen.dart
+++ lib/views/buyers/main_screen.dart
@@ -13,16 +13,16 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Main Screen'),),
+      body: Center(child: Text('Main Screen')),
       bottomNavigationBar: BottomNavigationBar(
         unselectedItemColor: Colors.black12,
-        selectedItemColor: Colors.orange,
+        selectedItemColor: Colors.blue,
         items: [
           BottomNavigationBarItem(icon: Icon(CupertinoIcons.home),label: 'HOME'),
           BottomNavigationBarItem(icon: SvgPicture.asset('assets/icons/account.svg',width: 20,),label: 'ACCOUNT'),
+          BottomNavigationBarItem(icon: SvgPicture.asset('assets/icons/cart.svg',width: 20,),label: 'CART'),
           BottomNavigationBarItem(icon: SvgPicture.asset('assets/icons/favorite.svg',width: 20,),label: 'FAVORITE'),
-          BottomNavigationBarItem(icon: SvgPicture.asset('assets/icons/shop.svg',width: 20),label: 'STORE'),
-          BottomNavigationBarItem(icon: SvgPicture.asset('assets/icons/search.svg',width: 20),label:'SEARCH')
+          BottomNavigationBarItem(icon: SvgPicture.asset('assets/icons/search.svg',width: 20,),label: 'SEARCH'),
         ],
       ),
     );
