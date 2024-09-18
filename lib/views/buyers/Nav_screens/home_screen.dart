3814025e3ce28fa3ed380f681a795b7095f3d7ff
TaiNgonheng <heng68807@gmail.com>
2024-09-18 15:17:11 +0700

    wrap row with with the column

--- lib/views/buyers/Nav_screens/home_screen.dart
+++ lib/views/buyers/Nav_screens/home_screen.dart
@@ -1,12 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home Screen'),
+    return Padding(
+      padding: EdgeInsets.only(
+          top: MediaQuery.of(context).padding.top, left: 25, right: 15),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Homiee, What are you\n looking for?👀',
+                style: TextStyle(
+                    fontSize: 19,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+
+              Container(
+                child: SvgPicture.asset('assets/icons/cart.svg', width: 20,),
+              )
+            ],
+          ),
+
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: TextField(
+              decoration: InputDecoration(
+                fillColor: Colors.white,
+                filled: true,
+                hintText: 'Search for product',
+                border: OutlineInputBorder(
+                  borderSide:BorderSide.none
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
