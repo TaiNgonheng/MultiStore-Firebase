68e04f5750a5847db4e8c234a57e9c9d8f54c892
TaiNgonheng <heng68807@gmail.com>
2024-09-22 17:55:06 +0700

    padding

--- lib/views/buyers/Nav_screens/widgets/welcome_text_widget.dart
+++ lib/views/buyers/Nav_screens/widgets/welcome_text_widget.dart
@@ -8,20 +8,23 @@ class WelcomeText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Text('Homiee, What are you\nlooking for?👀',
-          style: TextStyle(
-              fontSize: 19,
-              fontWeight: FontWeight.bold
+    return Padding(
+      padding:  EdgeInsets.only(top: MediaQuery.of(context).padding.top, left: 10, right: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text('Homiee, What are you\nlooking for?👀',
+            style: TextStyle(
+                fontSize: 19,
+                fontWeight: FontWeight.bold
+            ),
           ),
-        ),
 
-        Container(
-          child: SvgPicture.asset('assets/icons/cart.svg', width: 20,),
-        )
-      ],
+          Container(
+            child: SvgPicture.asset('assets/icons/cart.svg', width: 20,),
+          )
+        ],
+      ),
     );
   }
 }
