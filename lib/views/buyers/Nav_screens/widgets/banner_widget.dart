8e1883f9e8aca6665c28a6339433314c306383ff
TaiNgonheng <heng68807@gmail.com>
2024-09-22 18:02:28 +0700

    add banner page view

--- lib/views/buyers/Nav_screens/widgets/banner_widget.dart
+++ lib/views/buyers/Nav_screens/widgets/banner_widget.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class BannerWidget extends StatelessWidget {
@@ -5,12 +7,23 @@ class BannerWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 140,
-      width: MediaQuery.of(context).size.width,
-      decoration: BoxDecoration(
-        color: Colors.blue,
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Container(
+        height: 160,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.blue,
+          borderRadius: BorderRadius.circular(10)
 
+        ),
+        child: PageView(
+          children: [
+            Center(child: Text("Banner 1")),
+            Center(child: Text("Banner 2")),
+            Center(child: Text("Banner 3")),
+          ],
+        ),
       ),
     );
   }
