6d0ce837e0940e86408d4258584df19d3ae48047
TaiNgonheng <heng68807@gmail.com>
2024-09-22 17:54:38 +0700

    Bannner config

--- lib/views/buyers/Nav_screens/home_screen.dart
+++ lib/views/buyers/Nav_screens/home_screen.dart
@@ -17,7 +17,6 @@ class HomeScreen extends StatelessWidget {
           WelcomeText(),
           SizedBox(height: 10,),    
           SearchInputWidget(),
-
           BannerWidget(),
         ],
       ),
