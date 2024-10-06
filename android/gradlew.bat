68175c979ec9a970020814c7b7501452f619c76f
TaiNgonheng <heng68807@gmail.com>
2024-10-06 22:35:01 +0700

    graddle useful

--- android/gradlew.bat
+++ android/gradlew.bat
@@ -22,7 +22,6 @@ if defined JAVA_HOME goto findJavaFromJavaHome
 set JAVA_EXE=java.exe
 %JAVA_EXE% -version >NUL 2>&1
 if "%ERRORLEVEL%" == "0" goto init
-
 echo.
 echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
 echo.
