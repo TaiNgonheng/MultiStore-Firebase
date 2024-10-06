abbc00f8b5fa764faec779f5481cb2e7036785b6
TaiNgonheng <heng68807@gmail.com>
2024-10-06 22:35:24 +0700

    graddle useful

--- android/gradlew.bat
+++ android/gradlew.bat
@@ -32,7 +32,6 @@ goto fail
 set JAVA_HOME=%JAVA_HOME:"=%
 set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 if exist "%JAVA_EXE%" goto init
-
 echo.
 echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
 echo.
